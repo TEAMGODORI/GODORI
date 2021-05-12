701a4a4767d967d61a7661a964d72441685a9ba1
Song_a <o37o19@swu.ac.kr>
2021-05-12 22:21:36 +0900

    [Add] 온보딩뷰 수정 및 로그인뷰와 연결

--- GODORI/app/src/main/java/com/example/godori/activity/OnBoarding3Activity.kt
+++ GODORI/app/src/main/java/com/example/godori/activity/OnBoarding3Activity.kt
@@ -1,12 +1,21 @@
 package com.example.godori.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.godori.R
+import kotlinx.android.synthetic.main.activity_on_boarding2.*
+import kotlinx.android.synthetic.main.activity_on_boarding2.btn2
+import kotlinx.android.synthetic.main.activity_on_boarding3.*
 
 class OnBoarding3Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_boarding3)
+
+        btn3.setOnClickListener {
+            val intent = Intent(this, TabBarActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
